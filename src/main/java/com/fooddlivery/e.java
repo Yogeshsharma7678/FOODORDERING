18280a7e250412a9d81947b0477e1f933bfec40b
yogeshsharma7678 <yogeshmaithil7678@gmail.com>
2022-09-20 11:59:10 +0530

    Food Order

---
+++ src/main/java/com/fooddlivery/e.java
@@ -0,0 +1,24 @@
+package com.fooddlivery;
+
+public class e {
+	private int admin_id;
+	private String username;
+	
+	
+	public e(int admin_id, String username) {
+		super();
+		this.admin_id = admin_id;
+		this.username = username;
+	}
+
+
+	public int getAdmin_id() {
+		return admin_id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+	
+}
